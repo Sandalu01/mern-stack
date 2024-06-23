f8c5f2ca991c0258f470e36e4b7815e046a5a2da
sandaluthushan20 <sandaluthushan20@gmail.com>
2024-06-23 21:33:34 +0530

    s

--- App/index.js
+++ App/index.js
@@ -1,13 +1,23 @@
 
-var express = require('express');
-var mongoclient =require("mongodb").MongoClient;
-var core = require("cores");
-var multer = require("multer");
+var express = require(1,'express');
+var mongoclient =require(2,"mongodb").MongoClient;
+var core = require(3,"cores");
+var multer = require(4,"multer");
 
 //get express instance
  var app = express();
 
 app.use(core());
 
-var Connection_string ="mongodb+srv://sandaluthushan20:7XsATlPYwIkWMSfY@ransom.wxp1ukq.mongodb.net/?retryWrites=true&w=majority&appName=Ransom"
+var Connection_string ="mongodb+srv://sandaluthushan20:Mk67DS0NNLabb8ZI@cluster0.9qwykfs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
+var DATABASENAME ="APIDB";
+var database;
+
+
+app.listen(5038,()=>{
+    mongoclient.connect(Connection_string,(error,client)=>{
+        database=client.db(DATABASENAME);
+        console.log("connected to database");
+    })
+});
